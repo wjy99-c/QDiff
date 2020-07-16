e29f90e0ba9d83a302db91a9091cb9c2bffe86d0
Jiyuan Wang <wangjiyuan123@yeah.net>
2020-07-16 00:19:32 -0700

    finish compare results

--- beginTest/QTest.py
+++ beginTest/QTest.py
@@ -57,4 +57,3 @@ if __name__ == '__main__':
             print("Save document as:" + qiskitP2)
 
 
-
--- beginTest/compareResults.py
+++ beginTest/compareResults.py
@@ -5,12 +5,63 @@
 # @File    : compareResults.py
 
 import numpy as np
+import os
 
-def KSscore(r1:np.array(), r2:np.array()):
+def ks_score(r1, r2):
     r = r1-r2
     max_ks = max(max(r), 0)
     min_ks = min(min(r), 0)
     return (max_ks+min_ks)/r1.sum()
 
-def readResults(address: str,iteration:int):
-    
+def read_results(filename: str):
+    data = []
+    with open(filename, 'r') as f:
+        line = f.readline()
+        while line:
+            eachline = line.split()
+            data = [float(x) for x in eachline]
+            line = f.readline()
+    return data
+
+def compare(path:str, thershold:float):
+    data = []
+    name = []
+
+    files = os.listdir(path)
+
+    for file in files:
+        if not os.path.isdir(file):
+            data.append(read_results(path+"/"+file))
+            name.append(file)
+
+    candidates = []
+    answer = -1
+
+    for results in data:
+
+        flag = -1
+        for i in range(0,len(candidates)):
+            if ks_score(candidates[i]/(candidates[i].sum()),np.asarray(results))<thershold:
+                flag = i
+                candidates[i] = candidates[i] + np.asarray(results)
+                if candidates[i].sum() > candidates[answer].sum():
+                    answer = i
+                break
+
+        if flag == -1:
+            candidates.append(np.asarray(results))
+            if answer == -1:
+                answer = 0
+
+    wrong_out = []
+    max_diff = thershold
+    print("Right answer:"+candidates[answer])
+    for i in range(0, len(data)):
+        k = ks_score(np.asarray(data[i]),candidates[answer])
+        if k > thershold:
+            print(name[i]+":"+str(data[i]))
+            wrong_out.append(name[i])
+            if k > max_diff:
+                max_diff = k
+
+    return wrong_out, max_diff
