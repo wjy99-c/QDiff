851a4a0d4d24657e8cbcdfedb012aeb5d3f7fced
Jiyuan Wang <wangjiyuan123@yeah.net>
2020-07-24 16:29:57 -0700

    diff mutation initial

--- mutation/MutateQiskit_equal.py
+++ mutation/MutateQiskit_equal.py
@@ -9,7 +9,7 @@ import re
 
 def cnot_to_hczh(codeline:str):
     if re.search('prog.cnot', codeline) is not None:
-        return re.sub('CNOT', "H", codeline), re.sub('CNOT', "CZ", codeline), re.sub('CNOT',"H", codeline)
+        return re.sub('cnot', "h", codeline), re.sub('cnot', "cz", codeline), re.sub('cnot',"h", codeline)
     else:
         raise Exception('No CNOT gate for Swap transformation')
 
@@ -17,20 +17,20 @@ def order_change(codeline1:str, codeline2:str):
     return codeline2, codeline1
 
 def swap_to_cnot(codeline:str):
-    if re.search('SWAP', codeline) is not None:
-        return re.sub('SWAP', "CNOT", codeline), re.sub('SWAP', "CNOT", codeline), re.sub('SWAP',"CNOT", codeline)
+    if re.search('prog.swap', codeline) is not None:
+        return re.sub('swap', "cnot", codeline), re.sub('swap', "cnot", codeline), re.sub('swap',"cnot", codeline)
     else:
         raise Exception('No Swap gate for Swap transformation')
 
 def x_to_cnotxcnot(codeline:str):
-    if re.search('X', codeline) is not None:
-        return re.sub('X[(]', "CNOT(0,", codeline), codeline, re.sub(r'X[(]', "CNOT(0,", codeline)
+    if re.search('prog.x', codeline) is not None:
+        return re.sub('x[(]', "cnot(controls[0],", codeline), codeline, re.sub(r'x[(]', "cnot(controls[0],", codeline)
     else:
         raise Exception('No X gate for X transformation')
 
 def z_to_cnotzcnot(codeline:str):
-    if re.search('Z', codeline) is not None:
-        codeline1 = re.sub(r'Z', "CNOT", codeline)
-        return re.sub(r'[)]', ",0)", codeline1,count=1), codeline, re.sub(r'[)]', ",0)", codeline1,count=1)
+    if re.search('prog.z', codeline) is not None:
+        codeline1 = re.sub(r'z', "cnot", codeline)
+        return re.sub(r'[)]', ",controls[0])", codeline1,count=1), codeline, re.sub(r'[)]', ",controls[0])", codeline1,count=1)
     else:
         raise Exception('No Z gate for Z transformation')
